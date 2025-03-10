3060adf87549d0603a5314e6d8624d640d575c0b
Ngan <phamngocthungan20030@gmail.com>
2025-03-10 21:47:56 +0700

    install package for the app

--- app/src/main/java/com/example/shiso/MainActivity.kt
+++ app/src/main/java/com/example/shiso/MainActivity.kt
@@ -4,31 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.shiso.ui.theme.ShiSoTheme
+import ui.HomePage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ShiSoTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            HomePage()
         }
-    }
+        }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
---
+++ app/src/main/java/ui/HomePage.kt
@@ -0,0 +1,9 @@
+package ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+
+@Composable
+fun HomePage() {
+    Text("Welcome to Home Page!")
+}
